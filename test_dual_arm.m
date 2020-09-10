0abbf78d0a580b142e9527b2a37862eec797c2c3
Alessandro Palleschi <alessandropalleschi94@gmail.com>
2020-09-10 13:33:24 +0200

    Add existing file

--- test_dual_arm.m
+++ test_dual_arm.m
@@ -1,32 +1,37 @@
 clear
-robot_name = 'WRAPP_up';
+robot_name = 'AlterEgo';
 load(robot_name);
 close all
 
 q0 = robot.homeConfiguration;
 % q0 = q_0_right(2:7).';
 % tool = 'ee_link';
-Pright = [  [1.1995+0.1;   1.2710;   0.8580]];
+Pright = [  [0.0382;  -0.2591;   0.3217]];
 T0right = getTransform(robot.model,q0,robot.tool.right);
-quatright = rotm2quat(T0right(1:3,1:3)*rotz(-90));
+quatright = rotm2quat(T0right(1:3,1:3));
 
 
 
 
 T0left = getTransform(robot.model,q0,robot.tool.left);
-quatleft = rotm2quat(T0left(1:3,1:3)*rotz(-90));
-Pleft = [  [1.1995+0.05;   1.2710-0.9;   0.8580+0.01]];
+quatleft = rotm2quat(T0left(1:3,1:3));
+Pleft = [  [0.0382;  0.2591;   0.3217]];
 % T.left = repmat([T0(1:3,4);quat.'],[1,400]);
 [x,xd] = dualArmgenerateTrajectories(robot.model,q0,robot.tool.right,robot.tool.left,100,100,Pright,quatright,Pleft,quatleft);
 % T = repmat([P;quat.'],[1,200]);
+x.right = x.right(:,end);
+xd.right = xd.right(:,end);
+x.left = x.left(:,end);
+xd.left = xd.left(:,end);
+
+
 qout_RP = RP_inverse_dual_arm(x,xd,0.01,q0,robot.model,robot.limits.position,robot.tool);
-
-tic
-s = compute_s_joint(qout_RP);
-time_step = 0.01;
-[qopt,topt] = optimize_one_arm(qout_RP,s,time_step,robot);
-
-toc
+% tic
+% s = compute_s_joint(qout_RP);
+% time_step = 0.01;
+% [qopt,topt] = optimize_one_arm(qout_RP,s,time_step,robot);
+% 
+% toc
 figure()
 show(robot.model,qout_RP(:,end)')
 % light
